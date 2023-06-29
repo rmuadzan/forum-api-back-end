f7ad3fe81cca3d01f8fc3a77da5e5dab424d4a73
Rafli Muadzan <raflimuadzan2@gmail.com>
2023-06-29 11:01:21 +0800

    fix: add auth option in like comment route

--- src/Interfaces/http/api/thread_comments/routes.js
+++ src/Interfaces/http/api/thread_comments/routes.js
@@ -35,6 +35,9 @@ const routes = (handler) => ([
     method: 'PUT',
     path: '/threads/{threadId}/comments/{commentId}/likes',
     handler: handler.putCommentLikeHandler,
+    options: {
+      auth: 'forumapi_jwt',
+    },
   },
 ]);
 
