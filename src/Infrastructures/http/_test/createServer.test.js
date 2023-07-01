0075a38fd1758a03e2108795d61838e0f91f6046
rmuadzan <108398497+rmuadzan@users.noreply.github.com>
2023-07-01 09:08:33 +0800

    Add testing for root route

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -15,6 +15,21 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
+  it('should response 200 when access root url', async () => {
+    // Arrange
+    const server = await createServer({});
+
+    // Action
+    const response = await server.inject({
+      method: 'GET',
+      url: '/',
+    });
+
+    // Assert
+    expect(response.statusCode).toEqual(200);
+    expect(response.payload).toEqual('API for Open Music App');
+  });
+
   it('should handle server error correctly', async () => {
     // Arrange
     const requestPayload = {
